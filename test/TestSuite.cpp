556e109cf2dcf55eb96c41c229aff4e909966b38
Balint Kiss <balint.kiss.501@gmail.com>
2016-07-12 16:22:47 +0200

    Testing for UniquePtr storage overhead

--- test/TestSuite.cpp
+++ test/TestSuite.cpp
@@ -11,12 +11,19 @@ using namespace bkstl::Assert;
 
 void testUniquePtr()
 {
+    // Base unique pointer facilities
     bkstl::UniquePtr<int> int_ptr;
     if (!int_ptr)
     {
         int_ptr.reset(new int(100));
     }
     assertEquals(100, *int_ptr);
+
+    // Check for no storage overhead    FIXME: UniquePtr has storage overhead
+    //int* raw_intptr = new int(100);
+    //bkstl::UniquePtr<int> int_ptr2(new int(100));
+    //assertEquals(sizeof(raw_intptr), sizeof(int_ptr2), "UniquePtr has storage overhead");
+    //delete raw_intptr;
 }
 
 void testArrayList()
