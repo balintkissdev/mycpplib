8eb9710c8c046f134fe17496e49403fd93eee41c
balintkiss501 <balint.kiss.501@gmail.com>
2017-01-12 21:04:32 +0100

    Rebrand LinkedList to a doubly-linked list implementation

--- include/container/LinkedList.hpp
+++ include/container/LinkedList.hpp
@@ -10,34 +10,89 @@ namespace jlstl
 {
 
 template <typename T>
-struct SingleNode
-{
-  T value_;
-  SingleNode* next_node_;
-  SingleNode(T value, SingleNode* next = 0) : value_(value), next_node_(next) {};
-};
-
-template <typename T>
 class LinkedList : public List<T>
 {
   public:
+    struct DoubleNode
+    {
+      T value;
+      DoubleNode* previous_node;
+      DoubleNode* next_node;
+
+      DoubleNode()
+        : previous_node(NullPtr)
+        , next_node(NullPtr)
+        {}
+
+      DoubleNode(const T x)
+        : value(x)
+        , previous_node(NullPtr)
+        , next_node(NullPtr)
+        {}
+    };
+
     LinkedList() 
       : size_(0)
-      , head_node_(0)
+      , head_node_(NullPtr)
     {}
     ~LinkedList();
 
     bool add(const T element);
     void add(const int index, const T element);
+
+    /**
+     * Insert element to the beginning of linked list.
+     *
+     * @param element
+     */
+    void addFirst(const T element);
+
+    /**
+     * Append element to the end of linked list.
+     *
+     * @param element
+     */
+    void addLast(const T element);
+
     void clear();
     bool contains(const T& element) const;
     T get(const int index);
     T operator[](const int index);
+    
+    /**
+     * Return first element in linked list.
+     *
+     * @return
+     */
+    T getFirst();
+
+    /**
+     * Return last element in linked list.
+     *
+     * @return
+     */
+    T getLast();
+
     int indexOf(const T& element) const;
     bool isEmpty() const;
     int lastIndexOf(const T& element) const;
     T remove(const int index);
     bool remove(const T& element);
+
+    /**
+     * Remove and return first element from linked list.
+     *
+     * @return
+     */
+    T removeFirst();
+
+    /**
+     * Remove and return last element from linked list.
+     *
+     * @return
+     */
+    T removeLast();
+
     T set(const int index, const T& element);
     int size() const;
     void sort();
@@ -45,17 +100,18 @@ class LinkedList : public List<T>
 
   private:
     size_t size_;
-    SingleNode<T>* head_node_;
+    DoubleNode* head_node_;
 
     /**
      * Traverse until the end of list
      */
-    SingleNode<T>* traverse();
+    DoubleNode* traverse();
 
     /**
      * Traverse until specified index
      */
-    SingleNode<T>* traverse(const int index);
+    DoubleNode* traverse(const int index);
+
 };
 
 template <typename T>
@@ -70,12 +126,12 @@ inline bool LinkedList<T>::add(const T element)
   // Initial insert
   if (!head_node_)
   {
-    head_node_ = new SingleNode<T>(element);
+    head_node_ = new DoubleNode(element);
   }
   // Continous insert
   else
   {
-    traverse()->next_node_ = new SingleNode<T>(element);
+    traverse()->next_node = new DoubleNode(element);
   }
   ++size_;
 
@@ -89,15 +145,27 @@ inline void LinkedList<T>::add(const int index, const T element)
 }
 
 template <typename T>
+inline void LinkedList<T>::addFirst(const T element)
+{
+  // TODO
+}
+
+template <typename T>
+inline void LinkedList<T>::addLast(const T element)
+{
+  // TODO
+}
+
+template <typename T>
 inline void LinkedList<T>::clear()
 {
   // Start from beginning and delete everything from there
-  SingleNode<T>* current = head_node_;
-  SingleNode<T>* tmp = 0;
+  DoubleNode* current = head_node_;
+  DoubleNode* tmp = 0;
   while (current)
   {
     tmp = current;
-    current = tmp->next_node_;
+    current = tmp->next_node;
     delete tmp;
   }
   head_node_ = 0;
@@ -116,9 +184,23 @@ inline T LinkedList<T>::get(const int index)
 {
   if (-1 < index && index < size_)
   {
-    return traverse(index)->value_;
+    return traverse(index)->value;
   }
   throw std::exception();
+}
+
+template <typename T>
+inline T LinkedList<T>::getFirst()
+{
+  // TODO
+  return 0;
+}
+
+template <typename T>
+inline T LinkedList<T>::getLast()
+{
+  // TODO
+  return 0;
 }
 
 template <typename T>
@@ -162,6 +244,20 @@ inline bool LinkedList<T>::remove(const T& element)
 }
 
 template <typename T>
+inline T LinkedList<T>::removeFirst()
+{
+  // TODO
+  return 0;
+}
+
+template <typename T>
+inline T LinkedList<T>::removeLast()
+{
+  // TODO
+  return 0;
+}
+
+template <typename T>
 inline T LinkedList<T>::set(const int index, const T& element)
 {
   // TODO
@@ -188,26 +284,27 @@ inline T* LinkedList<T>::toArray()
 }
 
 template <typename T>
-SingleNode<T>* LinkedList<T>::traverse()
+inline typename LinkedList<T>::DoubleNode* LinkedList<T>::traverse()
 {
-  SingleNode<T>* current = head_node_;
-  while (current->next_node_)
+  DoubleNode* current = head_node_;
+  while (current->next_node)
   {
-    current = current->next_node_;
+    current = current->next_node;
   }
   return current;
 }
 
 template <typename T>
-SingleNode<T>* LinkedList<T>::traverse(const int index)
+inline typename LinkedList<T>::DoubleNode* LinkedList<T>::traverse(const int index)
 {
-  SingleNode<T>* current = head_node_;
+  DoubleNode* current = head_node_;
   for (int i = 0; i < index; ++i)
   {
-    current = current->next_node_;
+    current = current->next_node;
   }
   return current;
 }
+
 } // namespace jlstl
 
 #endif  // JLSTL_LINKED_LIST_HPP_
